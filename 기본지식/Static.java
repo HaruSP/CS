98f1da4bc2a8f73b1439fb8c3b99d513a4bcc163
haruSP <jeongseok0019@gmail.com>
2023-08-04 18:06:25 +0900

    fix: static 사용하지 않을때 예시

--- 기본지식/Static.java
+++ 기본지식/Static.java
@@ -22,9 +22,21 @@ public class Person {
         this.str = this.str + 1;
         System.out.println(this.name + "의 지능과 힘이 증가했습니다!" + this.IQ + " / " + this.str);
     }
+// static 사용하지 않을 시,
 // public void talk(Person a, Person b){
 //      System.out.println(a.name + " & " + b.name + "이 대화를 시작했다!");
 // }
+//     public static void main(String[] args) {
+//         Person a = new Person(); // 객체 >> 인스턴스
+//         a.levelup();
+//         Person b; // 객체
+//         b = new Person("큰돌", 1000, 1); // 인스턴스
+//         b.levelup();
+//         b.talk(b, a); 
+//         System.out.println(Person.GUDOC);
+//     }
+// }
+// static 사용시, 명시성 있는것을 확인, 메모리 효율성도 좋음
     private static void talk(Person a, Person b){
     System.out.println(a.name + " & " + b.name + "이 대화를 시작했다!");
     }
